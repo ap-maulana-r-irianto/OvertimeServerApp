6eb931be7e635773258982e011799f9fe8792c7f
Maulana Rafael Irianto <maulanarafael36@gmail.com>
2023-05-03 09:19:53 +0700

    first commit

---
+++ src/main/java/id/co/mii/overtimeserverapp/OvertimeServerAppApplication.java
@@ -0,0 +1,13 @@
+package id.co.mii.overtimeserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertimeServerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/mii/overtimeserverapp/OvertimeServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.mii.overtimeserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertimeServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
