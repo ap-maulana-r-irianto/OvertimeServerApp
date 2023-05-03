852331bd70d827e1ef36d57c1b4a927a4c20de23
Maulana Rafael Irianto <maulanarafael36@gmail.com>
2023-05-03 09:42:04 +0700

    Model Employee & User

---
+++ src/main/java/id/co/mii/overtimeserverapp/models/Employee.java
@@ -0,0 +1,46 @@
+package id.co.mii.overtimeserverapp.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+
+    @Column(unique = true, name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "email", nullable = false)
+    private String status;
+
+    @Column(name = "email", nullable = false)
+    private int payroll;
+
+    @OneToOne(mappedBy = "employee")
+    @PrimaryKeyJoinColumn
+    private User user;
+}
